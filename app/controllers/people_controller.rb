ff9474887a07daa9e2ca4c8dad58a32ebbace861
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-21 00:39:51 +0000

    automatically backed up by learn

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,10 +1,12 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
   end
 
-  def create    
-    Person.create(person_params)
+  def create
+    person = Person.create(person_params)
     redirect_to people_path
   end
 
@@ -17,4 +19,5 @@ class PeopleController < ApplicationController
   def person_params
     params.require(:person).permit(:name)
   end
+end
 end
